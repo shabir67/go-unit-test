cca3ea143bd970f6d27963b897578233c62a2a4a
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-05-31 14:12:13 +0700

    added new test called "TestHelloWorld"Assert" and "assert" libs from "stretchr/testify"

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,7 +3,16 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Prabowo")
+	assert.Equal(t, "Hello Prabowo", result, "Result must be 'Hello Prabowo'")
+	fmt.Println("Test HelloWorldAssert with Assert Done")
+
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Shobir")
