c04160b3fa86aa4dd4d78e8dc2da081803595031
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-05-31 13:54:30 +0700

    updated from "panic" to "fail" & "failnow" in two test that includes: 1.TestHelloWorld 2.TestHelloworldJoko

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,24 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Shobir")
 	if result != "Hello Shobir" {
-		// unit test failed
-		panic("Result is not Hello Shobir")
+		// error
+		t.Fail()
 	}
+	fmt.Println("Test HelloWorld Shobir Done")
+}
+
+func TestHelloWorldJoko(t *testing.T) {
+	result := HelloWorld("Joko")
+	if result != "Hello Joko" {
+		// error
+		t.FailNow()
+	}
+	fmt.Println("Test HelloWorld Joko Done")
 }
